10e9499eeb289e91a694ddd8453c981c2e745e6f
BunSu <vuongquoctuan98@gmail.com>
2024-11-01 19:59:15 +0700

    Create medical_record.js

---
+++ medical_record.js
@@ -0,0 +1,4 @@
+document.getElementById('view-results-button').addEventListener('click', function() {
+    alert('Hiển thị kết quả Cận lâm sàng cho bệnh nhân Vương Quốc Tuấn.');
+    // Bạn có thể chuyển hướng tới trang kết quả cụ thể ở đây nếu cần
+});
