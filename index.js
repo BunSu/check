7673a58e9aa03dc7c004b280b16e233941dd633d
BunSu <vuongquoctuan98@gmail.com>
2024-11-01 19:58:40 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById('send-button').addEventListener('click', function() {
+    // Thay thế bằng logic gửi mã OTP thực tế
+    alert('Mã OTP đã được gửi!');
+});
+
+document.getElementById('verify-button').addEventListener('click', function() {
+    // Thay thế bằng logic xác nhận mã OTP thực tế
+    alert('Mã OTP đã được xác nhận!');
+});
